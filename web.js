b77f620e568afe7a87c6b96d1dbbba9e2bd3802d
Quim Castella <quim.castella@gmail.com>
2013-07-14 21:13:01 +0000

    output from index.html

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var output = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(output.toString());
 });
 
 var port = process.env.PORT || 5000;
